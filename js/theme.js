725811739cbdd85bf24d54987c8c23fcb13e67b2
toptulla <ad.tsay@outlook.com>
2022-05-15 08:22:19 +0000

    Deploying to gh-pages from @ toptulla/PomodoroTimer@3125db3838186b0a2161ca43369a5c9a43466d72 🚀

---
+++ js/theme.js
@@ -0,0 +1,9 @@
+﻿window.SwitchTheme = () => {
+    let htmlCalssName = document.documentElement.className;
+    if (htmlCalssName !== "dark") {
+        document.documentElement.classList.add('dark');
+    }
+    else {
+        document.documentElement.classList.remove('dark');
+    }
+}
